aa20c213135ea2464574e72eef02cc0dee7401f3
abdullahmia <abdullah149161@bsdi-bd.org>
2024-02-28 01:38:15 +0600

    fix: resolve docs routes in server

--- src/app.ts
+++ src/app.ts
@@ -51,17 +51,8 @@ app.use(
   })
 );
 
-// TODO: Test endpoint for testing the CI/CD pipeline. Remove this endpoint in production
-app.get('/', (req, res) => {
-  return res.status(200).json({
-    message: 'Welcome to the API',
-    status: 'success',
-    code: 200
-  });
-});
-
 // api docs
-app.use('/docs', swagger.serve, swagger.setup(specs));
+app.use('/api/docs', swagger.serve, swagger.setup(specs));
 
 // jwt authentication
 app.use(passport.initialize());
